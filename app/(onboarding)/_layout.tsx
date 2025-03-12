e2a8b29f5d10b967b7299f0349b657302e920275
Jason <jkb5bg@virginia.edu>
2025-03-12 09:39:21 -0400

    Putting the general scaffolding in place

---
+++ app/(onboarding)/_layout.tsx
@@ -0,0 +1,9 @@
+export default function OnboardingLayout() {
+    return (
+
+        <>
+        
+        </>
+
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,42 @@
 import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, View } from 'react-native';
+
 
 export default function RootLayout() {
-  return (
-    <>
-      <Stack>
-        <StatusBar hidden={true} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </>
-  );
+
+  const hasCompletedOnboarding = true;
+
+  if (hasCompletedOnboarding) {
+    return (
+      <View style={{ flex: 1, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+        <StatusBar hidden={false} translucent style="light" />
+        <Stack
+          screenOptions={{
+            headerShown: true, // or true depending on what you want
+            contentStyle: { backgroundColor: 'black' }, // Optional
+          }}
+        >
+          <Stack.Screen name="(onboarding)" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </View>
+    );
+  }
+  else {
+    return (
+      <View style={{ flex: 1, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+        <StatusBar hidden={false} translucent style="light" />
+        <Stack
+          screenOptions={{
+            headerShown: true, // or true depending on what you want
+            contentStyle: { backgroundColor: 'black' }, // Optional
+          }}
+        >
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </View>
+    );
+  }
 }
